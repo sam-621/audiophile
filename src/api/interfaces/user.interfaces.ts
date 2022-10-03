5582c1057f5f8c64743c2880c5ee9af88cb58158
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-10-02 22:35:29 -0600

    add dtos

--- src/api/interfaces/user.interfaces.ts
+++ src/api/interfaces/user.interfaces.ts
@@ -1,4 +1,9 @@
 import { IUser } from '@/shared/interfaces/user.interface'
 
+// Inputs
 export type ISignUpInput = Pick<IUser, 'name' | 'email' | 'password'>
 export type ISignInInput = Pick<IUser, 'email' | 'password'>
+
+// Dto
+export type ISignUpDto = ISignUpInput
+export type ISignInDto = ISignInInput
