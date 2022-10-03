95e17302d8fa234d884aec524d4a6f7368d9d957
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-10-02 19:28:33 -0600

    update db connection

--- src/api/db/connection.ts
+++ src/api/db/connection.ts
@@ -1,15 +1,11 @@
 import { MongoClient } from 'mongodb'
 
 if (!process.env.MONGO_DB_URI) {
-  throw new Error('Invalid environment variable: "MONGO_DB_URI"')
+  throw new Error('Please add your Mongo URI to .env.local')
 }
 
 const uri = process.env.MONGO_DB_URI
 const options = {}
-
-if (!process.env.MONGO_DB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local')
-}
 
 const client = new MongoClient(uri, options)
 const clientPromise = client.connect()
