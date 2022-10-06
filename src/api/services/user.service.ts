7e0d813aef0eb51aeed6a4773432a107d082abbd
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-10-05 23:21:46 -0600

    fix typo

--- src/api/services/user.service.ts
+++ src/api/services/user.service.ts
@@ -1,13 +1,13 @@
-import { ServiceResult } from '@/api-interfaces/utils.interface'
+import { ServiceResponse } from '@/api-interfaces/utils.interface'
 import { UserRepository } from '@/api-repositories/user.repository'
 import { IUser } from '@/shared/interfaces/user.interface'
 import { TMongoId } from '@/shared/interfaces/utils'
 
 export class UserService {
-  static async getProfile(id: TMongoId): Promise<ServiceResult<IUser>> {
+  static async getProfile(id: TMongoId): Promise<ServiceResponse<IUser>> {
     const userRepository = new UserRepository()
     const userProfile = await userRepository.findOneById<IUser>(id)
 
-    return new ServiceResult(userProfile, 'OK', 200)
+    return new ServiceResponse(userProfile, 'OK', 200)
   }
 }
