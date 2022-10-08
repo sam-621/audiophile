168a78c177930f1f91c409b74e1a452f52bede10
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-10-08 14:39:28 -0600

    add signInHandler

--- src/api/validations/auth.validation.ts
+++ src/api/validations/auth.validation.ts
@@ -3,8 +3,8 @@ import Joi from 'joi'
 
 export const signUpValidator = (input: TSignUpDto): string[] | undefined => {
   const schema = Joi.object<TSignUpDto>({
-    firstName: Joi.string().alphanum().min(3).max(30).required(),
-    lastName: Joi.string().alphanum().min(3).max(30).required(),
+    firstName: Joi.string().min(3).max(30).required(),
+    lastName: Joi.string().min(3).max(30).required(),
     email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
     password: Joi.string().min(6)
   })
--- src/pages/api/auth/sign-in.ts
+++ src/pages/api/auth/sign-in.ts
@@ -1,1 +1,22 @@
-export {}
+import { HttpStatusCodes } from '@/api-constants/status-codes'
+import { AuthService } from '@/api-services/auth.service'
+import { is404Endpoint } from '@/api-utils/request'
+import { signInValidator } from '@/api-validations/auth.validation'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const signInHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (is404Endpoint(req, 'POST')) {
+    return res.status(HttpStatusCodes.NOT_FOUND).json({ message: 'NOT FOUND' })
+  }
+
+  const errors = signInValidator(req.body)
+
+  if (Boolean(errors?.length)) {
+    return res.status(HttpStatusCodes.BAD_REQUEST).json({ message: errors })
+  }
+
+  const token = await AuthService.signIn(req.body)
+  res.status(HttpStatusCodes.OK).json(token)
+}
+
+export default signInHandler
