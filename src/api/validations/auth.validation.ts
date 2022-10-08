0979bee7607999a3fb7e4d8bd23785408666de57
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-10-08 14:26:30 -0600

    add signInValidator

--- src/api/validations/auth.validation.ts
+++ src/api/validations/auth.validation.ts
@@ -1,10 +1,21 @@
-import { TSignUpDto } from '@/api-interfaces/user.interfaces'
+import { TSignInDto, TSignUpDto } from '@/api-interfaces/user.interfaces'
 import Joi from 'joi'
 
 export const signUpValidator = (input: TSignUpDto): string[] | undefined => {
   const schema = Joi.object<TSignUpDto>({
     firstName: Joi.string().alphanum().min(3).max(30).required(),
     lastName: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    password: Joi.string().min(6)
+  })
+
+  const { error } = schema.validate(input)
+
+  return error?.details.map((item) => item.message)
+}
+
+export const signInValidator = (input: TSignInDto): string[] | undefined => {
+  const schema = Joi.object<TSignInDto>({
     email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
     password: Joi.string().min(6)
   })
--- src/pages/api/auth/sign-up.ts
+++ src/pages/api/auth/sign-up.ts
@@ -1,4 +1,4 @@
-import { StatusCodes } from '@/api-constants/status-codes'
+import { HttpStatusCodes } from '@/api-constants/status-codes'
 import { AuthService } from '@/api-services/auth.service'
 import { is404Endpoint } from '@/api-utils/request'
 import { signUpValidator } from '@/api-validations/auth.validation'
@@ -6,17 +6,17 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 const signUpHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (is404Endpoint(req, 'POST')) {
-    return res.status(StatusCodes.NOT_FOUND).json({ message: 'NOT FOUND' })
+    return res.status(HttpStatusCodes.NOT_FOUND).json({ message: 'NOT FOUND' })
   }
 
   const errors = signUpValidator(req.body)
 
   if (Boolean(errors?.length)) {
-    return res.status(StatusCodes.BAD_REQUEST).json({ message: errors })
+    return res.status(HttpStatusCodes.BAD_REQUEST).json({ message: errors })
   }
 
   const token = await AuthService.signUp(req.body)
-  res.status(StatusCodes.OK).json(token)
+  res.status(HttpStatusCodes.OK).json(token)
 }
 
 export default signUpHandler
