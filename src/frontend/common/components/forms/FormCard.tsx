a56bf9a81d5f91b58fbfa50c7580cdc3642bcf81
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-10-20 23:35:23 -0600

    integrate FormCard

--- src/frontend/common/components/forms/FormCard.tsx
+++ src/frontend/common/components/forms/FormCard.tsx
@@ -1,9 +1,15 @@
 import { FC, ReactNode } from 'react'
 
-export const FormCard: FC<Props> = ({ children }) => {
-  return <div className="p-6">{children}</div>
+export const FormCard: FC<Props> = ({ children, title }) => {
+  return (
+    <div className="p-6">
+      <h2 className="text-left text-3xl font-bold">{title}</h2>
+      <div>{children}</div>
+    </div>
+  )
 }
 
 type Props = {
   children: ReactNode
+  title: string
 }
--- src/frontend/modules/sign-up/SignUpView.tsx
+++ src/frontend/modules/sign-up/SignUpView.tsx
@@ -1,9 +1,30 @@
 import { AuthHeader } from '@/front-components/auth/AuthHeader'
+import { Input } from '@/front-components/custom/Input/Input'
+import { FormCard } from '@/front-components/forms/FormCard'
 
 export const SignUpView = () => {
   return (
     <>
       <AuthHeader />
+      <FormCard title="Sign Up">
+        <Input
+          variant="text"
+          type="text"
+          label="Name"
+          placeholder="Insert your name"
+          value="sam"
+          onChange={() => null}
+          error="hi"
+        />
+        <Input
+          variant="text"
+          type="text"
+          label="Name"
+          placeholder="Insert your name"
+          value="sam"
+          onChange={() => null}
+        />
+      </FormCard>
     </>
   )
 }
