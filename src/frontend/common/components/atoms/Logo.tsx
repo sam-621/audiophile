84dfedf95d964aba1b8f369de1d1d90b3e441114
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-10-20 23:19:51 -0600

    add logo component

---
+++ src/frontend/common/components/atoms/Logo.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export const LogoLight = () => {
+  return <Image src="/img/logo-light.svg" alt="Logo" />
+}
+
+export const LogoDark = () => {
+  return <Image src="/img/logo-dark.svg" alt="Logo" />
+}
