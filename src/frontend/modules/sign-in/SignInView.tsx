e5e4f78d8ef7b89d9e7de315922af2f0125d8696
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-10-21 22:28:49 -0600

    add SignInForm

--- src/frontend/modules/sign-in/SignInView.tsx
+++ src/frontend/modules/sign-in/SignInView.tsx
@@ -1,31 +1,11 @@
 import { AuthHeader } from '@/front-components/auth/AuthHeader'
-import { Button } from '@/front-components/custom/Button/Button'
-import { Form } from '@/front-components/custom/Form'
-import { Input } from '@/front-components/custom/Input/Input'
-import { useSignIn } from './hooks/useSignIn'
+import { SignInForm } from './components/SignInForm'
 
 export const SignInView = () => {
-  const { onSubmit, register, errors } = useSignIn()
   return (
     <>
       <AuthHeader />
-      <Form title="SIGN IN" className="flex flex-col gap-6" onSubmit={onSubmit}>
-        <Input
-          register={register('email')}
-          type="email"
-          label="Email"
-          placeholder="Insert your email"
-          error={errors.email?.message}
-        />
-        <Input
-          register={register('password')}
-          type="password"
-          label="Password"
-          placeholder="Insert your password"
-          error={errors.password?.message}
-        />
-        <Button variant="base" text="Submit" />
-      </Form>
+      <SignInForm />
     </>
   )
 }
