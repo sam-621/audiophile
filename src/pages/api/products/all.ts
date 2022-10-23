6eb448e4874dd663356b3abd3bc6c5ae081b9d8b
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-10-23 17:23:08 -0600

    add handler for all products

--- src/pages/api/products/all.ts
+++ src/pages/api/products/all.ts
@@ -1,1 +1,10 @@
-export {}
+import { HandlerResponse } from '@/api-interfaces/utils.interface'
+import { ProductService } from '@/api-services/products.service'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const handler = async (_: NextApiRequest, res: NextApiResponse) => {
+  const { data, message, status } = await ProductService.getAllProducts()
+  return new HandlerResponse(data, message, status, res)
+}
+
+export default handler
