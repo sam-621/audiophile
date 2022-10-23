c1e2a30ed2c4c4808b1476fadd6ac0829c4a93f5
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-10-23 17:52:53 -0600

    add forgot password btn

--- src/frontend/modules/sign-in/components/SignInForm.tsx
+++ src/frontend/modules/sign-in/components/SignInForm.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/front-components/custom/Button/Button'
 import { Form } from '@/front-components/custom/Form'
 import { Input } from '@/front-components/custom/Input/Input'
+import Link from 'next/link'
 import { useSignIn } from '../hooks/useSignIn'
 
 export const SignInForm = () => {
@@ -15,13 +16,22 @@ export const SignInForm = () => {
         placeholder="Insert your email"
         error={errors.email?.message}
       />
-      <Input
-        register={register('password')}
-        type="password"
-        label="Password"
-        placeholder="Insert your password"
-        error={errors.password?.message}
-      />
+      <div>
+        <Input
+          register={register('password')}
+          type="password"
+          label="Password"
+          placeholder="Insert your password"
+          error={errors.password?.message}
+        />
+        <div className="flex justify-end">
+          <Link href="/forgot-password">
+            <a href="/forgot-password" className="text-xs text-primary font-bold mt-2">
+              FORGOT PASSWORD?
+            </a>
+          </Link>
+        </div>
+      </div>
       <Button variant="base" text="Submit" disabled={isLoading} />
     </Form>
   )
