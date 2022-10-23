e26ea4d61dfd145752f58a3cbea639dbc2efb3b5
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-10-22 21:16:59 -0600

    remove loader logic

--- src/frontend/common/components/custom/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import LinearProgress from '@material/react-linear-progress'
-
-export const Loader = () => {
-  return (
-    <div className="fixed top-0 left-0 z-50 w-full">
-      <LinearProgress />
-    </div>
-  )
-}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { Session } from 'next-auth'
-
-import '../frontend/styles/globals.css'
-import 'tailwindcss/tailwind.css'
 import { AppLayout } from '@/front-components/layouts/AppLayout'
+
+import 'tailwindcss/tailwind.css'
+import '../frontend/styles/globals.css'
 
 const MyApp = ({
   Component,
