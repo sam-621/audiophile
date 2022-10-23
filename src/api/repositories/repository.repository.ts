d7e49249d3c27a5b717eb28e197d984fb6666e42
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-10-23 17:04:59 -0600

    gave documentation for Repository class

--- src/api/repositories/repository.repository.ts
+++ src/api/repositories/repository.repository.ts
@@ -18,6 +18,11 @@ export class Repository {
     return collection
   }
 
+  /**
+   * Method which search by id in the collection given
+   * @param id mongo id
+   * @returns Document
+   */
   async findOneById<T = Document>(id: TMongoId): Promise<TEntityWithId<T>> {
     const collection = await this.getDbCollection()
     const result = await collection.findOne({ _id: new ObjectId(id) })
@@ -25,6 +30,11 @@ export class Repository {
     return result as unknown as TEntityWithId<T>
   }
 
+  /**
+   * Method which search by the condition given
+   * @param filter Filter applied to the search
+   * @returns Document
+   */
   async findOneByFilter<T = Document>(filter: Filter<T>): Promise<TEntityWithId<T>> {
     const collection = await this.getDbCollection()
     const result = await collection.findOne(filter)
@@ -32,6 +42,10 @@ export class Repository {
     return result as unknown as TEntityWithId<T>
   }
 
+  /**
+   * Method which returns all documents found in the collection given
+   * @returns Array of documents
+   */
   async find<T = Document>(): Promise<TEntityWithId<T>[]> {
     const collection = await this.getDbCollection()
     const result = await collection.find().toArray()
@@ -39,11 +53,22 @@ export class Repository {
     return result as unknown as TEntityWithId<T>[]
   }
 
+  /**
+   * Method which insert a document into the collection given
+   * @param input Document to insert
+   * @returns Document inserted
+   */
   async insert(input: Document): Promise<InsertOneResult<Document>> {
     const collection = await this.getDbCollection()
     return collection.insertOne(input)
   }
 
+  /**
+   * Method which update a document which match with the id given
+   * @param id Document id to update
+   * @param input Data to update
+   * @returns Document updated
+   */
   async update(id: TMongoId, input: Document): Promise<ModifyResult<Document>> {
     const collection = await this.getDbCollection()
     return collection.findOneAndUpdate(id, input)
