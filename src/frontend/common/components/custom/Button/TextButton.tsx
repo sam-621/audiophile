6913833b030805a9adbe798c308d57f5cf0dd0bb
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-10-23 18:20:28 -0600

    import all photos

--- src/frontend/common/components/custom/Button/TextButton.tsx
+++ src/frontend/common/components/custom/Button/TextButton.tsx
@@ -11,7 +11,7 @@ export const TextButton: FC<Props> = ({ text, link, className, onClick }) => {
           className={`flex items-center gap-2 text-xs bg-none text-subtitle hover:text-primary transition ${className}`}
         >
           <span>{text}</span>
-          <Image src="/img/icons/arrow-right.svg" alt="Arrow right" width={5} height={10} />
+          <Image src="/icons/arrow-right.svg" alt="Arrow right" width={5} height={10} />
         </a>
       </Link>
     )
@@ -23,7 +23,7 @@ export const TextButton: FC<Props> = ({ text, link, className, onClick }) => {
       onClick={onClick}
     >
       <span>{text}</span>
-      <Image src="/img/icons/arrow-right.svg" alt="Arrow right" width={5} height={10} />
+      <Image src="/icons/arrow-right.svg" alt="Arrow right" width={5} height={10} />
     </button>
   )
 }
