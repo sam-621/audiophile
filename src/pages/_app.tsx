62a10773a3775f1b7ae07928b644ab563b21ceb7
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-10-26 00:07:27 -0600

    add react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,23 @@
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { Session } from 'next-auth'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import 'tailwindcss/tailwind.css'
 import '../frontend/styles/globals.css'
+
+export const queryClient = new QueryClient()
 
 const MyApp = ({
   Component,
   pageProps: { session, ...pageProps }
 }: AppProps<{ session: Session }>) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
 
