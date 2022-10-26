f759815602a3e7f78a1fb92935d500c1508a89f8
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-10-26 00:17:46 -0600

    add ServiceResponse

--- src/frontend/common/interfaces/services/http-request.interface.ts
+++ src/frontend/common/interfaces/services/http-request.interface.ts
@@ -1,7 +1,19 @@
 import { THashMap } from '@/shared/interfaces/utils'
+import { AxiosResponse } from 'axios'
+import { Response } from '../utils.interface'
 
 export type TRequestOptions = {
   endpoint: string
   apiDomain?: string
   headers?: THashMap
+}
+
+export class ServiceResponse<T> {
+  data: T
+  response: AxiosResponse<Response<T>>
+
+  constructor(data: T, response: AxiosResponse) {
+    this.data = data
+    this.response = response
+  }
 }
