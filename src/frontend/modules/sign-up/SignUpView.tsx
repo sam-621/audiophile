00efc8b87fb58ec7b51d676a1ee9d93c307e6c20
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-10-30 18:01:08 -0700

    resolve tsc error

--- src/frontend/modules/sign-up/SignUpView.tsx
+++ src/frontend/modules/sign-up/SignUpView.tsx
@@ -1,47 +1,9 @@
 import { AuthHeader } from '@/front-components/auth/AuthHeader'
-import { Button } from '@/front-components/custom/Button/Button'
-import { Input } from '@/front-components/custom/Input/Input'
-import { FormCard } from '@/front-components/custom/Form'
 
 export const SignUpView = () => {
   return (
     <>
       <AuthHeader />
-      <FormCard title="SIGN UP" className="flex flex-col gap-6">
-        <Input
-          variant="text"
-          type="text"
-          label="First Name"
-          placeholder="Insert your first name"
-          value=""
-          onChange={() => null}
-        />
-        <Input
-          variant="text"
-          type="text"
-          label="Last Name"
-          placeholder="Insert your last name"
-          value=""
-          onChange={() => null}
-        />
-        <Input
-          variant="text"
-          type="text"
-          label="Email"
-          placeholder="Insert your email"
-          value=""
-          onChange={() => null}
-        />
-        <Input
-          variant="text"
-          type="text"
-          label="Password"
-          placeholder="Insert your password"
-          value=""
-          onChange={() => null}
-        />
-        <Button variant="base" text="Submit" />
-      </FormCard>
     </>
   )
 }
