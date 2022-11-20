5d40e624c1346328cc540bfa628ec5e30bccc846
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-11-20 12:03:40 -0700

    add responsive to About

--- src/frontend/common/components/sections/About.tsx
+++ src/frontend/common/components/sections/About.tsx
@@ -1,22 +1,19 @@
-import Image from 'next/image'
-
+/* eslint-disable @next/next/no-img-element */
 export const About = () => {
   return (
-    <section className="flex flex-col items-center">
-      <div className="mb-10">
-        <Image
+    <section className="flex flex-col items-center lg:flex-row-reverse lg:gap-32">
+      <div className="mb-10 flex-grow">
+        <img
           src="/img/shared/desktop/image-best-gear.jpg"
-          width={327}
-          height={300}
           alt="Best gear"
-          className="object-cover rounded-lg"
+          className="object-cover rounded-lg flex-grow"
         />
       </div>
-      <div>
-        <h3 className="font-bold text-3xl text-center mb-8">
+      <div className="lg:w-1/2">
+        <h3 className="font-bold text-3xl text-center mb-8 lg:text-left">
           BRINGING YOU THE <strong className="text-primary">BEST</strong> AUDIO GEAR
         </h3>
-        <p className="text-center font-medium text-subtitle">
+        <p className="text-center font-medium text-subtitle lg:text-left">
           Located at the heart of New York City, Audiophile is the premier store for high end
           headphones, earphones, speakers, and audio accessories. We have a large showroom and
           luxury demonstration rooms available for you to browse and experience a wide range of our
