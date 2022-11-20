f6e81a2b605bb9bae1778bdc15307278601ba454
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-11-20 01:12:02 -0700

    add responsive to category card

--- src/frontend/common/components/items/CategoryCard.tsx
+++ src/frontend/common/components/items/CategoryCard.tsx
@@ -5,13 +5,14 @@ import { Button } from '../custom'
 
 export const CategoryCard: FC<Props> = ({ product }) => {
   return (
-    <div className="bg-cards rounded-lg grid pb-6 h-44">
+    <div className="bg-cards rounded-lg grid pb-6 h-44 flex-grow">
       <div className="flex justify-center flex-col relative bottom-14">
         <div className="flex justify-center">
           <Image
             width={150}
             height={150}
             src={`/img/shared/desktop/image-category-thumbnail-${product}.png`}
+            alt={product}
           />
         </div>
         <h3 className="text-center m-0 mb-4 font-bold" style={{ letterSpacing: 1 }}>
--- src/frontend/common/components/sections/Categories.tsx
+++ src/frontend/common/components/sections/Categories.tsx
@@ -2,7 +2,7 @@ import { CategoryCard } from '../items'
 
 export const Category = () => {
   return (
-    <section className="grid gap-16 mt-20">
+    <section className="grid gap-16 mt-20 md:flex md:justify-center">
       <CategoryCard product="headphones" />
       <CategoryCard product="speakers" />
       <CategoryCard product="earphones" />
