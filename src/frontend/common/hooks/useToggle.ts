00280f2fc62009ec6efab023aa4bb2c42918e88a
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-11-26 13:05:49 -0700

    add useToggle

---
+++ src/frontend/common/hooks/useToggle.ts
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+export const useToggle = (initialValue = false) => {
+  const [state, setState] = useState(initialValue)
+
+  const toggle = () => setState(!state)
+
+  return {
+    state,
+    setState,
+    toggle
+  }
+}
