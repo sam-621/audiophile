1e4fc42c7b50e025a47f18e8a6fe106021c3ae4e
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-11-29 14:12:45 -0700

    add getProductCategoryName

--- src/frontend/common/utils/products.ts
+++ src/frontend/common/utils/products.ts
@@ -1,5 +1,16 @@
-import { IProduct } from '@/shared/interfaces/product'
+import { IProduct, ProductCategory } from '@/shared/interfaces/product'
+import { THashMap } from '@/shared/interfaces/utils'
+
+const PRODUCT_CATEGORIES: THashMap = {
+  [ProductCategory.EARPHONES]: 'EARPHONES',
+  [ProductCategory.HEADPHONES]: 'HEADPHONES',
+  [ProductCategory.SPEAKERS]: 'SPEAKERS'
+}
 
 export const getNewProducts = (products: IProduct[]) => {
   return products?.filter(product => Boolean(product.new))
+}
+
+export const getProductCategoryName = (category: ProductCategory) => {
+  return PRODUCT_CATEGORIES[category] ?? ''
 }
